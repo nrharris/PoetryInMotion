a8b8d71103445e505439b56689977f3478e4bf76
nrharris <nrharris@unca.edu>
2013-01-07 15:55:56 -0500

    Working rhyming methods.  Now with more Object Orientation.

--- poetryMethods.py
+++ poetryMethods.py
@@ -1,26 +1,41 @@
 import nltk
+import re
 
-pronunciationDict = nltk.corpus.cmudict.dict()
+class Rhymer():
+	#Rhymer deals with words and their sounds
 
-def rhymes(firstWord,secondWord):	
-	#returns true if a firstWord rhymes with secondWord
-	#Currently only works with last syllable rhyming
-	
-	for fword in pronunciationDict[firstWord]:
-		for sword in pronunciationDict[secondWord]:
-			if fword[-1] == sword[-1]:
-				return True
-	return False
+	def __init__(self):
+		self.pronunciationDict = nltk.corpus.cmudict.dict()
 
-def generateRhymingWords(word):
-	#returns a list of words rhyming with word
-	#obviously horribly inefficient
+	def lastStressed(self,word):
+		#returns the index of the lass stressed syllable
+		#expects a list of syllables
+		
+		index = len(word)-1
+		pattern = re.compile("[1-9]")
 
-	suffixes = set([wordv[-1] for wordv in pronunciationDict[word]])
-	rhymingWords = []
-	for eachWord in pronunciationDict:
-		for wordSuffix in pronunciationDict[eachWord]:
-			if wordSuffix[-1] in suffixes:
-				rhymingWords.append(eachWord)
-	return rhymingWords
+		for i in reversed(range(len(word))):
+			if pattern.search(word[i]):
+				return i
+		return index
+
+	def rhymes(self,firstWord,secondWord):	
+		#returns true if a firstWord rhymes with secondWord
+		#only returns perfect rhymes
+				
+		for pronunciation in self.pronunciationDict[firstWord]:
+			index = self.lastStressed(pronunciation)
+			for pronun in self.pronunciationDict[secondWord]:
+				if pronun[index:] == pronunciation[index:]:
+					return True		
+		return False
+
+	def generateRhymingWords(self,word):
+		#returns a list of words rhyming with word
+		
+		wordList = []
+		for rword in self.pronunciationDict:
+			if self.rhymes(word,rword):
+				wordList.append(rword)
+		return wordList
 
