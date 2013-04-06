734aed914e07204d0da138877205cdee0715e165
nrharris <nrharris@unca.edu>
2013-04-06 17:07:16 -0400

    converting tagset from CLAWS7 to PENN

--- dataCreation/populateData.py
+++ dataCreation/populateData.py
@@ -1,4 +1,6 @@
 import sqlite3
+import csv
+import re
 from wordMethods.sounds import SyllableCounter
 
 class DatabaseInit:
@@ -20,6 +22,10 @@ class DatabaseInit:
 		self.segmentList = {}
 		self.posList = {}
 		self.bigramList = {}
+		self.conversion = {}
+
+		for row in csv.reader(open("data/tagset/conversion.csv","rb")):
+			self.conversion[row[0]] = row[1]
 
 	def TablesInit(self):
 		self.cursor.execute('''CREATE TABLE FiveSyllables
@@ -43,13 +49,11 @@ class DatabaseInit:
 				self.SyllableTablesInit(line,i)
 
 				if i == 3:  #only use 5grams data since using everything takes too long
-					self.BuildPosTable(line)
 					self.BuildBigramTable(line)
 
 			self.connection.commit()
 			f.close()
 		
-		self.PosTablesInit()
 		self.BigramTablesInit()
 		
 	def SyllableTablesInit(self,line,fileNumber):
@@ -73,68 +77,6 @@ class DatabaseInit:
 			self.cursor.execute("INSERT into SevenSyllables VALUES (?,?)",(self.sevenCount,culledLine))
 			
 		
-	def BuildPosTable(self,line):
-		'''
-		   creates a dictionary of dictionaries
-		   upper tier' keys denote the first part of speech
-		   and lower tier keys represent parts of speech that follow.
-		   values are the frequencies with which the pos pairings occur
-		''' 
-		splitLine = line.split("\t")
-		count = int(splitLine[0])
-		partsOfSpeech = [word.strip("\t\r\n") for word in splitLine[6:]] 	
-		
-		for i in range(len(partsOfSpeech)-1):
-			pos = partsOfSpeech[i].strip("\t\r\n ")
-			nextPos = partsOfSpeech[i+1].strip("\t\r\n ")
-
-			if pos not in self.posList:
-				self.posList[pos] = {}
-				self.posList[pos][nextPos] = count
-			else:
-				if nextPos not in self.posList[pos]:
-					self.posList[pos][nextPos] = count
-				else:
-					self.posList[pos][nextPos]+= count
-			
-			if "totSize" not in self.posList[pos]:
-				self.posList[pos]["totSize"] = count
-			else:
-				self.posList[pos]["totSize"] += count
-	
-		#self.posSize+=count	
-				
-	def PosTablesInit(self):
-		'''
-		    takes the data created in BuildPosTable, 
-		    normalizes the frequency, and inserts a copy of
-		    the dictionary into the database	
-		'''
-		newPosList = {}
-		
-		for key,partsOfSpeech in self.posList.items():
-			newPosList[key] = {}
-
-			for pos,value in partsOfSpeech.items():
-				if pos!="totSize":
-					freq = (value/(self.posList[key]["totSize"]+0.0))
-					
-					if freq * 100 < 1:
-						freq = .01
-					else:
-						freq = float("{0:.2f}".format(freq))
-
-					newPosList[key][pos] = freq
-
-		count = 0
-		for key,partsOfSpeech in self.posList.items():
-			for pos,value in partsOfSpeech.items():
-				count+=1
-				self.cursor.execute("INSERT into PosBigrams VALUES (?,?,?,?)",
-						(count,key,pos,value))
-		
-		self.connection.commit()	
-
 	
 	def BuildBigramTable(self,line):
 		splitLine = [word.strip("\t\r\n ") for word in line.split("\t")]
@@ -143,8 +85,8 @@ class DatabaseInit:
 		for i in xrange(1,5):
 			firstWord = splitLine[i]
 			secondWord = splitLine[i+1]
-			firstPos = splitLine[i+5]
-			secondPos = splitLine[i+6]
+			firstPos = self.convertPos(splitLine[i+5])
+			secondPos = self.convertPos(splitLine[i+6])
 			
 			if firstWord not in self.bigramList:
 				self.bigramList[firstWord] = {}
@@ -182,11 +124,14 @@ class DatabaseInit:
 		#print self.bigramList	
 
 
+	def convertPos(self,pos):
+		pos = re.sub("\d+","",pos).upper()
 
-
-
-
-
+		if pos in self.conversion:
+			return self.conversion[pos]
+		else:
+			return pos
+			
 
 
 
--- poetry.py
+++ poetry.py
@@ -1,3 +1,23 @@
 from wordMethods.haiku import createEvolvedHaiku
 
 print createEvolvedHaiku()
+
+'''
+from dataCreation.populateData import DatabaseInit
+
+d = DatabaseInit()
+
+d.TablesInit()
+'''
+
+'''
+import sqlite3
+
+conn = sqlite3.connect("data/haiku.db")
+c = conn.cursor()
+
+for row in c.execute("select firstPos,firstWord from Bigrams"):
+	print row[0]
+	print row[1]	
+
+'''
